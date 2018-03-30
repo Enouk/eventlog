4714f8461931134ad451906215acdf91c2ce12a2
Enouk <tandy.nilsson@gmail.com>
2018-03-30 13:59:53 +0200

    Added sample data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,13 +5,15 @@ angular.module('eventsApp')
 
     $scope.events = [{
       Start: '2018-03-04 18:11:40',
-      End: '2018-03-04 18:11:40',
+      End: '2018-03-04 18:11:45',
+      Duration: '5sec 3ms',
       Classification: 'GOOD',
       Samples: '30',
       Image: ''
     }, {
-      Start: '2018-03-04 18:11:40',
-      End: '2018-03-04 18:11:40',
+      Start: '2018-03-04 18:11:47',
+      End: '2018-03-04 18:11:53',
+      Duration: '5sec 3ms',
       Classification: 'BAD',
       Samples: '25',
       Image: 'sdsdd'
